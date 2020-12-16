c1f7f76cd9f35cecf14eb7943e985bb610405cbb
RG161997 <71373566+RG161997@users.noreply.github.com>
2020-12-16 16:02:37 +0530

    Add files via upload

---
+++ CalculatorController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Models;
+
+namespace Calculator.Controllers
+{
+    
+    public class CalculatorController : Controller
+    {
+        
+        // GET: Calculator
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult total(string number1, string number2, string calc)
+        {
+            decimal num1 = Convert.ToDecimal(number1.ToString());
+            decimal num2 = Convert.ToDecimal(number2.ToString());
+            decimal res = 0.0M;
+            switch (calc)
+            {
+                case "Add":
+                    res = num1 + num2;
+                    break;
+                case "Subtract":
+                    res = num1 - num2;
+                    break;
+                case "Multiply":
+                    res = num1 * num2;
+                    break;
+                case "Divide":
+                    try
+                    {
+                        res = num1 / num2;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        return Content("Divide by 0 is not allowed");
+                    }
+                    break;
+                default:
+                    Response.Write("Invalid input");
+                    break;
+
+            }
+            return Content("Result is : " + res);
+        }
+    }
+}
+
+    }
+}
---
+++ SimpleCalc.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Calculator.Controllers;
+
+namespace Calculator.Models
+{
+    public class SimpleCalc
+    {
+
+        public decimal number1
+        {
+            get;
+            set;
+        }
+        public decimal number2
+        {
+            get;
+            set;
+        }
+        public string op
+        {
+            get;
+           
+
+        }
+        public decimal result
+        {
+            get;
+            set;
+        }
+        public decimal Add(decimal leftNumber,decimal rightNumber)
+        {
+            return (leftNumber + rightNumber);
+        }
+        public decimal Sub(decimal leftNumber,decimal rightNumber)
+        {
+            return (leftNumber - rightNumber);
+        }
+        public decimal Mult(decimal leftNumber, decimal rightNumber)
+        {
+            return (leftNumber * rightNumber);
+        }
+        public decimal Div(decimal leftNumber, decimal rightNumber)
+        {
+            return (leftNumber / rightNumber);
+        }
+        public decimal OperatorSwitch(string op,decimal leftNumber,decimal rightNumber)
+        {
+            decimal res=0.0M;
+            switch (op)
+            {
+                case "Add":
+                    res = Add(leftNumber,rightNumber);
+                    break;
+                case "Subtract":
+                    res = Sub(leftNumber, rightNumber);
+                    break;
+                case "Multiply":
+                    res = Mult(leftNumber, rightNumber);
+                    break;
+                case "Divide":
+                    try
+                    {
+                        res = Div(leftNumber, rightNumber);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Divide by 0 not allowed");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("invalid operator");
+                    break;
+
+            }
+            return res;
+        }
+    }
+    }
